90de735128e412dbe472a73509b28c19d7c15db3
nivedh <nivedh0203@gmail.com>
2023-10-05 09:51:59 +0530

    end of day 2

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,8 +3,24 @@ const router =express.Router();
 
 //routes
 
-router.get('',(req,res) => {                   //users the router instead of the app
-    res.send("hello world");
+router.get('',(req,res) => {                   //uses the router instead of the app
+
+    const locals = {
+        title: "Node Blog",
+        description: "simple blog"
+    }
+
+    res.render('index', {locals});   //renders the index pagein layouts
+});
+
+router.get('/about',(req,res) => {                   //uses the router instead of the app
+    res.render('about');   //renders the about pagein layouts
+});
+
+
+router.get('/contact', (req, res) => {
+    // Handle the GET request for /contact here
+    res.render('contact');
 });
 
 module.exports = router;
