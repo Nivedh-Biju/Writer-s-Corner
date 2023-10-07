59a3e9cd184da8622b6c1105eecfdd7508ce9909
nivedh <nivedh0203@gmail.com>
2023-10-07 17:35:16 +0530

    blog project finale commit

--- app.js
+++ app.js
@@ -1,31 +1,50 @@
 require('dotenv').config();
 
 const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
+const expressLayout = require('express-ejs-layouts');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
-const app=express();
-const PORT = 5000 || prosecc.env.PORT //this is to set default port to 5000, in case of hosting we will use their default port
+const connectDB = require('./server/config/db');
+const { isActiveRoute } = require('./server/helpers/routeHelpers');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+  
+// Connect to DB
+connectDB();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride('_method'));
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI
+  }),
+  //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
+}));
+
+app.use(express.static('public'));
+
+// Templating Engine
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
 
 
+app.locals.isActiveRoute = isActiveRoute; 
 
 
-app.use(express.static('public'));  //this uses the folder called public as the static part to access css,html,js,imgs etc
+app.use('/', require('./server/routes/main'));
+app.use('/', require('./server/routes/admin'));
 
-
-
-//templating engine
-
-app.use(expressLayouts);     // using the layouts of express for html
-app.set('layout','./layouts/main');      //setting default layouts for the website from the folder ./layouts/main
-app.set('view engine','ejs');    //setting view engine
-
-
-// app.get('',(req,res) => {
-//     res.send("hello world");
-// });
-
-app.use('/',require('./server/routes/main'));      //tells the file to use for starting the app
-
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
+app.listen(PORT, ()=> {
+  console.log(`App listening on port ${PORT}`);
 });
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,20 +1,110 @@
 const express = require('express');
 const router =express.Router();
-
+const Post = require('../models/Post');
 //routes
 
-router.get('',(req,res) => {                   //uses the router instead of the app
+router.get('',async (req,res) => {                   //uses the router instead of the app
+
+   //renders the index pagein layouts
+
+try {
 
     const locals = {
         title: "Node Blog",
         description: "simple blog"
     }
 
-    res.render('index', {locals});   //renders the index pagein layouts
+    //res.render('index', {locals});
+
+    let perPage = 10;
+    let page = req.query.page||1;        //if page number not present in web search query, then default is 1
+
+    const data = await Post.aggregate([{ $sort: {createdAt: -1}}])
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec();
+
+
+    const count = await Post.count();
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count/perPage);
+
+    //const data= await Post.find();
+    res.render('index', {locals,
+        data,
+        current: page,
+        nextPage: hasNextPage ? nextpage : null
+    });
+} catch (error) {
+    console.log(error);
+}
+
 });
 
-router.get('/about',(req,res) => {                   //uses the router instead of the app
-    res.render('about');   //renders the about pagein layouts
+
+
+
+// function insertPostData (){
+//     Post.insertMany([
+//         {
+//             title: "Building a block",
+//             body: "This is the body text"
+//         },
+//     ])
+// }
+
+// insertPostData();
+
+
+
+
+
+
+
+router.get('/post/:id', async (req, res) => {
+    try {
+
+        let slug = req.params.id; // Corrected variable name
+
+        const data = await Post.findById({ _id: slug }); // Corrected variable name
+
+        const locals = {
+            title: data.title,
+            description: "simple blog post",
+        }
+
+        res.render('post', { locals, data }); // Corrected object structure
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+
+router.post('/search', async (req, res) => {
+    try {
+        const locals = {
+            title: "search",
+            description: "simple blog"
+        }
+
+        let searchTerm = req.body.searchTerm;
+        const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9]/g,"");
+
+        const data= await Post.find({
+            $or: [
+                {title: { $regex: new RegExp(searchNoSpecialChar, 'i')}},
+                {body: { $regex: new RegExp(this.searchNoSpecialChar,'i')}}
+            ]
+        });
+        console.log(searchTerm);
+
+        res.render('search',{
+            data,
+            locals
+        })
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 
@@ -23,4 +113,8 @@ router.get('/contact', (req, res) => {
     res.render('contact');
 });
 
+router.get('/about', (req, res) => {
+    // Handle the GET request for /contact here
+    res.render('about');
+});
 module.exports = router;
